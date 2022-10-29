c99efa9ad072e535c02b674ec9439854e3743100
btomecsko <brandon.tomecsko92@gmail.com>
2022-10-29 17:12:01 -0500

    added a search functionality

--- src/components/MoviesContainer.js
+++ src/components/MoviesContainer.js
@@ -6,6 +6,15 @@ import Movies from "./Movies";
 const MoviesContainer = () => {
 
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("")
+
+  const searchMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  function handleChange(e) {
+    setSearchTerm(e.target.value);
+  }
 
   useEffect(() => {
     fetch('http://localhost:3001/lgbtqMovies')
@@ -16,8 +25,13 @@ const MoviesContainer = () => {
 
   return (
     <Container fluid>
+    <div>
+    <input className="prompt" value={searchTerm} onChange={handleChange} />
+        <i className="search icon" />
+    </div>
+    <div>
     <ul className="cards">
-      {movies.map(movie => (
+      {searchMovies.map(movie => (
         <Movies
         key={movie.id}
         title={movie.title}
@@ -27,6 +41,7 @@ const MoviesContainer = () => {
         />
       ))}
     </ul>
+    </div>
     </Container>
   );
 }
