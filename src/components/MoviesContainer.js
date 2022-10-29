511aa53819338294fb221e0e32a49095cb6f38f6
btomecsko <brandon.tomecsko92@gmail.com>
2022-10-29 18:58:01 -0500

    installed react styled components

--- src/components/MoviesContainer.js
+++ src/components/MoviesContainer.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react";
 
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+
 
 import Movies from "./Movies";
 
